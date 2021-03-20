3463b205968d5fac4ad7a0f5112bfc9655a10948
Satvik Reddy <reddy.satvik@gmail.com>
2021-03-19 20:39:34 -0700

    added code for file uploads

--- notescribe/__init__.py
+++ notescribe/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
+UPLOAD_FOLDER = './test_uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from notescribe.views import *
--- notescribe/views/index.py
+++ notescribe/views/index.py
@@ -1,6 +1,17 @@
 from notescribe import app
-from flask import render_template
+from flask import render_template, request
+from werkzeug.utils import secure_filename
+import os.path
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print('no file')
+            return render_template('index.html')
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        print('upload successful')
+        return render_template('index.html')
     return render_template('index.html')
