aede07b9de76da5e9a8327376c7895c1eee71650
Niels Voss <nvosscode@gmail.com>
2021-03-20 14:42:55 -0700

    Add page turning functionality
    Co-authored-by: Satvik Reddy <SatvikR@users.noreply.github.com>

--- notescribe/static/js/music.js
+++ notescribe/static/js/music.js
@@ -1,3 +1,7 @@
+// globals
+let currentImg = 0;
+let imgs = [];
+
 /**
  * Get json url from page url
  * @returns {string} 
@@ -17,15 +21,17 @@ const getData = (jsonUrl) => {
 	.then((data) => {
 		document.getElementById("loading").remove();
 		console.log(data.image_urls)
-		
-		const musicDiv = document.getElementById('music');
-		for (let i = 0; i < data.image_urls.length; i++) {
-			const img = document.createElement('img');
-			img.src = data.image_urls[i];
-			img.id = i;
-			musicDiv.appendChild(img)
-			musicDiv.appendChild(document.createElement('hr'))
-		}
+		imgs = data.image_urls;		
+		updateImage()
+
+		// const musicDiv = document.getElementById('music');
+		// for (let i = 0; i < data.image_urls.length; i++) {
+		// 	const img = document.createElement('img');
+		// 	img.src = data.image_urls[i];
+		// 	img.id = i;
+		// 	musicDiv.appendChild(img)
+		// 	musicDiv.appendChild(document.createElement('hr'))
+		// }
 	})
 }
 
@@ -35,6 +41,31 @@ const getData = (jsonUrl) => {
 const loadMusic = () => {
 	jsonUrl = setup();
 	getData(jsonUrl)
+}
+
+const previousPage = () => {
+	if (currentImg > 0) {
+		currentImg--;
+	}
+	updateImage()
+}
+
+const nextPage = () => {
+	if (currentImg < imgs.length - 1) {
+		currentImg++;
+	}
+	updateImage()
+
+}
+
+const updateImage = () => {
+	if (!document.getElementById('current')) {
+		const current = document.createElement('img')
+		current.id = 'current'
+		document.getElementById('music').appendChild(current)
+	}
+	const imageUrl = imgs[currentImg]
+	document.getElementById('current').setAttribute('src', imageUrl)
 }
 
 loadMusic();
