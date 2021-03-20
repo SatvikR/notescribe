fc9e6874f7ff693e93b5b8532d7cbdd182a9e82a
Niels Voss <nvosscode@gmail.com>
2021-03-19 23:29:14 -0700

    Add lilypond conversion
    Co-authored-by: Satvik Reddy <SatvikR@users.noreply.github.com>

--- notescribe/__init__.py
+++ notescribe/__init__.py
@@ -2,8 +2,10 @@ import json
 from flask import Flask
 import os.path
 
-UPLOAD_FOLDER = './object_storage/uploads'
-MIDI_FOLDER = './object_storage/midi'
+UPLOAD_FOLDER = os.path.join('object_storage', 'uploads')
+MIDI_FOLDER = os.path.join('object_storage', 'midi')
+LILYPOND_FOLDER = os.path.join('object_storage', 'lilypond')
+
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
--- notescribe/process.py
+++ notescribe/process.py
@@ -1,3 +1,8 @@
+from notescribe import settings, MIDI_FOLDER, LILYPOND_FOLDER
+import subprocess
+import os.path
+import os
+
 def process_file(file_hash, upload_filename) -> bool:
     '''
     Processes a user upload file so that it can be sent to the client
@@ -7,16 +12,29 @@ def process_file(file_hash, upload_filename) -> bool:
     # Convert to midi
     midi_filename = convert_to_midi(file_hash, upload_filename)
     print(f'midi_filename: {midi_filename}')
+    lilypond_filename = convert_to_lilypond(file_hash, midi_filename)
+    print(f'lilypond_filename: {lilypond_filename}')
 
     return False
 
-def convert_to_midi(file_hash: str, filename) -> str:
+def convert_to_midi(file_hash: str, upload_filename) -> str:
     '''
     Converts a user uploaded file to midi.
     Returns True upon success, False otherwise
     '''
     # TODO: Call machine learning code
-    print(f'Converting file {filename} with hash {file_hash}')
+    print(f'Converting file {upload_filename} with hash {file_hash}')
 
     # Return example midi file
     return 'midi_123456789abcdefexample.mid'
+
+def convert_to_lilypond(file_hash: str, midi_filename) -> str:
+    filename = os.path.join(MIDI_FOLDER, midi_filename)
+
+    lilypond_path = settings['lilypond_path']
+    if not os.path.isdir(os.path.join(LILYPOND_FOLDER)):
+        os.makedirs(os.path.join(LILYPOND_FOLDER))
+
+    output_file = os.path.join(LILYPOND_FOLDER, f'midi_{file_hash}.mid')
+    subprocess.run(['python', lilypond_path, filename, '-o', output_file])
+    return output_file
