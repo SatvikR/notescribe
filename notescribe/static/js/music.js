73806eaadbb99c215544daf79f15d68dff9b2573
Niels Voss <nvosscode@gmail.com>
2021-03-20 14:06:38 -0700

    Added loading functionality to music page
    Co-authored-by: Satvik Reddy <SatvikR@users.noreply.github.com>

--- notescribe/static/js/music.js
+++ notescribe/static/js/music.js
@@ -1,25 +1,40 @@
-// setup
-const jsonUrl = new URLSearchParams(window.location.search).get('url')
-let isLoading = true;
+/**
+ * Get json url from page url
+ * @returns {string} 
+ */
+const setup = () => {
+	const jsonUrl = new URLSearchParams(window.location.search).get('url')
+	return jsonUrl;
+}
 
-// get data
-fetch(jsonUrl)
+/**
+ * Fetches data from s3
+ * @param {string} jsonUrl
+ */
+const getData = (jsonUrl) => {
+	fetch(jsonUrl)
 	.then((res) => res.json())
 	.then((data) => {
-		isLoading = false;
+		document.getElementById("loading").remove();
 		console.log(data.image_urls)
 		
 		const musicDiv = document.getElementById('music');
-		// data.image_urls.foreach((e, i) => {
-		// 	const img = document.createElement('img')
-		// 	img.src = e;
-		// 	img.id = i;
-		// 	musicDiv.appendChild(img)
-		// })
 		for (let i = 0; i < data.image_urls.length; i++) {
 			const img = document.createElement('img');
 			img.src = data.image_urls[i];
 			img.id = i;
 			musicDiv.appendChild(img)
+			musicDiv.appendChild(document.createElement('hr'))
 		}
 	})
+}
+
+/**
+ * Main method that runs when page is loaded
+ */
+const loadMusic = () => {
+	jsonUrl = setup();
+	getData(jsonUrl)
+}
+
+loadMusic();
